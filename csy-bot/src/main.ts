d164d8074c5879fbceb84759878bf606766cb8cb
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2025-09-27 18:33:09 +0300

    csy-bot

---
+++ csy-bot/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as process from "process";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT);
+}
+bootstrap().then(()=>{
+  console.log(`app.listen(${process.env.PORT});`)
+});
